e8ab6172ff91ec558d8adbd0e44ef5caabbb90b9
Winney360 <nkathawinnie94@gmail.com>
2025-06-04 13:38:39 +0300

    Updated insert_books.js

--- insert_books.js
+++ insert_books.js
@@ -4,7 +4,7 @@
 const { MongoClient } = require('mongodb');
 
 // Connection URI (replace with your MongoDB connection string if using Atlas)
-const uri = 'mongodb://localhost:27017';
+const uri = 'mongodb+srv://mongoUser:345pass@clustertron.ww4apwr.mongodb.net/';
 
 // Database and collection names
 const dbName = 'plp_bookstore';
